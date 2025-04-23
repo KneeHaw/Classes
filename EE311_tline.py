8e72e1082cb37e4b1e995b363815b67e36b9d2ff
KneeHaw <90938928+KneeHaw@users.noreply.github.com>
2025-04-23 09:24:36 -0500

    Add files via upload

---
+++ EE311_tline.py
@@ -0,0 +1,24 @@
+# Define parameters given or not given
+Zl = None
+Zg = None
+Zo = None
+Vp0 = None
+Vn0 = None
+VpL = None
+VnL = None
+V = None
+Ip0 = None
+In0 = None
+IpL = None
+InL = None
+I = None
+length = None
+
+def main():
+    a = 1 + 2j
+    b = 2 + 1j
+    print(a + b)
+
+
+if __name__ == "__main__":
+    main()
